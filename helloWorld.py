7615f34cd0063d316b835f427498dc095c947275
MayconAguiar <maycon.aguiar@gmail.com>
2018-10-04 10:34:58 -0300

    primeiro commit

---
+++ helloWorld.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  3 22:16:57 2018
+
+@author: Maycon
+"""
+
+a=5
+b=5
+
+import numpy as np
+np.max([3,5])
+
+import sys
+sys.version
+
+import tensorflow
+tensorflow.VERSION
+
+import keras
+keras.__version__
